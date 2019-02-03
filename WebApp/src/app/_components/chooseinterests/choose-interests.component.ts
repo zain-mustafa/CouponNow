03bcf02e8e60fe3df81b235023a98a5e9565f49a
Dillon Oudomsouk <oudomsod@uwindsor.ca>
2019-02-03 15:13:30 -0500

    create wireframe for choose interests appearance

--- WebApp/src/app/_components/chooseinterests/choose-interests.component.ts
+++ WebApp/src/app/_components/chooseinterests/choose-interests.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
+
 @Component({
   selector: 'app-choose-interests',
   templateUrl: './choose-interests.component.html',
@@ -7,9 +14,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChooseInterestsComponent implements OnInit {
 
+  breakpoint: number;
+  minCols: number = 1;
+  maxCols: number = 3;
+
   constructor() { }
 
   ngOnInit() {
+    this.breakpoint = (window.innerWidth <= 400) ? this.minCols : this.maxCols;
   }
+
+  onResize(event) {
+    this.breakpoint = (event.target.innerWidth <= 400) ? this.minCols : this.maxCols;
+  }
+
+  tiles: Tile[] = [
+    {text: 'Restaurants', cols: 1, rows: 1, color: 'lightblue'},
+    {text: 'Clothes', cols: 1, rows: 1, color: 'lightblue'},
+    {text: 'Fitness', cols: 1, rows: 1, color: 'lightblue'},
+    {text: 'Art', cols: 1, rows: 1, color: 'lightblue'},
+    {text: 'Music', cols: 1, rows: 1, color: 'lightblue'},
+    {text: 'Other', cols: 1, rows: 1, color: 'lightblue'},
+  ];
 
 }
